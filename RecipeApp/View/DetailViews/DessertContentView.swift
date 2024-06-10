8920236a751ff697c1a95e385ebb9a8b37b4f645
samgusa <samgusaapp1993@gmail.com>
2024-06-10 17:41:45 -0400

    Created the basic main contentView

---
+++ RecipeApp/View/DetailViews/DessertContentView.swift
@@ -0,0 +1,44 @@
+//
+//  DessertDetailView.swift
+//  RecipeApp
+//
+//  Created by Sam Greenhill on 6/10/24.
+//
+
+import SwiftUI
+
+struct DessertContentView: View {
+
+    let dessertModel: DessertModel
+
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            AsyncImage(url: URL(string: dessertModel.mealImage)) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 100)
+                } else if phase.error != nil {
+                    Color.red // Indicates an error
+                        .frame(width: 100, height: 100)
+                } else {
+                    Color.gray // Acts as a placeholder
+                        .frame(width: 100, height: 100)
+                }
+            }
+            Text(dessertModel.mealName)
+                .font(.caption)
+                .padding([.top, .bottom], 10)
+        }
+        .background(Color.gray.opacity(0.2))
+        .cornerRadius(10)
+    }
+}
+
+#Preview {
+    DessertContentView(dessertModel: DessertModel(mealName: "Chocolate Cake",
+                                                 mealImage: "https://www.themealdb.com/images/media/meals/adxcbq1619787919.jpg",
+                                                 mealId: "1"))
+}
