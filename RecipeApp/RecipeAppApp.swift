e4d5af5917640d95e7f63190e446bbca3dcf5dab
samgusa <samgusaapp1993@gmail.com>
2024-06-10 17:00:08 -0400

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Sam Greenhill on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
